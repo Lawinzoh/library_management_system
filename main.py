3730d18f2384017b03787629226f897cac3d442a
Lawrence <lawrenceowino96@gmail.com>
2025-08-11 12:03:49 -0700

    feat(lms): Create a test file

---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+from models.book import Book
+from models.library import Library
+from models.users import Member
+from services.library_service import LibraryService
+
+
+def main():
+    # Create infrastructure
+    library = Library("My Local library")
+    library_service = LibraryService(library)
+
+    #Add books
+    books_list= []
+    with open("./data/books.json", "r") as file:
+        books_list = json.load(file)
+
+    for book in books_list:
+        library.add_book(book=Book(book["title"], book["author"], book["isbn"]))
+
+    #Register users
+    user = Member("Lawinzoh")
+    library.register_user(user)
+
+    try:
+        library_service.checkout_book(user, "To Kill a Mockingbird")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
